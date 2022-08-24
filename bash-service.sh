d9bef93e366a3de13b9319aa87387b80ef0c5445
ekkyrp <40424207+prabowoekky@users.noreply.github.com>
2022-08-24 23:47:07 +0700

    Update bash-service.sh

--- bash-service.sh
+++ bash-service.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 kubectl config use-context azure-aksDRC
-service-drc=$(kubectl get svc | grep python | awk '{print $3}')
+export service-drc=$(kubectl get svc | grep python | awk '{print $3}')
 echo $service-drc
 sed -i 's/service-drc/$service-drc/g' balancing-service.yaml
 kubectl config use-context eq-aksDC
-service-dc=$(kubectl get svc | grep python | awk '{print $3}')
+export service-dc=$(kubectl get svc | grep python | awk '{print $3}')
 echo $service-dc
 sed -i 's/service-dc/$service-dc/g' balancing-service.yaml
 kubectl apply -f balancing-service.yaml
