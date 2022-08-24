e76ff89220457efa5eabcf3bfd6b412f15ae762b
ekkyrp <40424207+prabowoekky@users.noreply.github.com>
2022-08-24 23:50:40 +0700

    Update bash-service.sh

--- bash-service.sh
+++ bash-service.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 kubectl config use-context azure-aksDRC
-export service-drc=$(kubectl get svc | grep python | awk '{print $3}')
+export service-drc=$(kubectl get svc python-app-svc-drc -ojsonpath='{.spec.clusterIP}')
 echo $service-drc
 sed -i 's/service-drc/$service-drc/g' balancing-service.yaml
 kubectl config use-context eq-aksDC
-export service-dc=$(kubectl get svc | grep python | awk '{print $3}')
+export service-dc=$(kubectl get svc python-app-svc-dc -ojsonpath='{.spec.clusterIP}')
 echo $service-dc
 sed -i 's/service-dc/$service-dc/g' balancing-service.yaml
 kubectl apply -f balancing-service.yaml
